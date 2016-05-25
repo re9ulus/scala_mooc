bfe9b609c62a9de205cf7fc18a9ddd63844faec3
Vasiliy Zemlyanov <angeldaren@gmail.com>
2016-05-25 21:41:51 +0300

    Update, add gitinore, remove all files from tracking

--- HelloWorld/build.sbt
+++
@@ -1,6 +0,0 @@
-name := "HelloWorld"
-
-version := "1.0"
-
-scalaVersion := "2.11.8"
-    
--- HelloWorld/src/main/scala/example/Example.scala
+++
@@ -1,8 +0,0 @@
-package example
-
-/**
-  * Created by Vasiliy on 24.05.2016.
-  */
-object Example extends App {
-  println("Hello, World!")
-}
